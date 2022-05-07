352181f9b4b3add3660d3c3efc406645ceb17cb7
salamone-gius <salamone.gius@gmail.com>
2022-05-07 11:33:36 +0200

    palindroma - function wordFlip created

--- palindroma/main.js
+++ palindroma/main.js
@@ -3,25 +3,24 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 // 1. chiedo all'utente di inserire una parola
-const word = String(prompt("Inserisci una parola.", "Ti dirò se è palindroma."));
-console.log(typeof word);
-console.log(word.length);
+let askWord = String(prompt(`${"Inserisci una parola."} ${"Ti dirò se è palindroma."}`));
+console.log(typeof askWord);
+console.log(askWord.length);
 
-// 2. dichiaro una variabile vuota COME STRINGA
-let reverseWord = "";
-
-// 3. ciclo la parola per la sua lunghezza (numero di caratteri) partendo dall'ultima
-for (let i = word.length - 1; i >= 0; i--) {
-    console.log(word[i]);
-    // 3.1 aggiungo ogni lettera di word alla variabile reverseWord per ottenere la parola al rovescio
-    reverseWord = reverseWord + word[i];
-    // 2.1 SE la prima lettera è uguale all'ultima, la seconda alla penultima, e così via...
-    // if (word[i] === word[word.length - 1 - i]) {
-    //     console.log("Palindroma");
-    // } else {
-    //     console.log("Non palindroma");
-    // }
+// 2. creo una funzione che mi restituisce la parola inserita dall'utente al rovescio
+function wordFlip() {
+    // 2.1 dichiaro una variabile vuota e VOGLIO CHE SIA UNA STRINGA per poterla gestire (+ o -) come un array
+    let flippedWord = "";
+    // 2.2 ciclo la parola per la sua lunghezza - 1 (numero di caratteri) partendo dall'ultima
+    for (let i = askWord.length - 1; i >= 0; i--) {
+        console.log(askWord[i]);
+        // 2.3 aggiungo ogni lettera di word alla variabile reverseWord per ottenere la parola al rovescio
+        flippedWord += askWord[i];
+    }
+    return flippedWord
 }
 
+// 3. salvo il risultato della funzione wordFlip nella const reverseWord
+const reverseWord = wordFlip(askWord);
 console.log(reverseWord);
-                               
+// debugger;                           
